4aaf8d6fc40d7299db760b23ac9075148a2cd6d2
Pawel Zalejko <p.zalejko@gmail.com>
2021-01-07 21:52:07 +0100

    set the main class and logger

--- core/src/main/java/com/gmail/pzalejko/weather/DefaultSensorService.java
+++ core/src/main/java/com/gmail/pzalejko/weather/DefaultSensorService.java
@@ -3,17 +3,31 @@ package com.gmail.pzalejko.weather;
 import com.pi4j.component.temperature.TemperatureSensor;
 import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
 import com.pi4j.io.w1.W1Master;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 class DefaultSensorService implements SensorService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultSensorService.class);
+
+
     @Override
     public List<Sensor> getSensors() {
+        LOG.debug("Searching sensors...");
         var w1Devices = new W1Master().getDevices(TmpDS18B20DeviceType.FAMILY_CODE);
-        return w1Devices.stream()
+        List<TemperatureSensor> list = w1Devices.stream()
                 .map(i -> (TemperatureSensor) i)
+                .collect(Collectors.toList());
+
+        LOG.debug("Found {} sensors", list.size());
+        for (var sensor : list) {
+            LOG.debug("Found sensor: {}", sensor.toString());
+        }
+
+        return list.stream()
                 .map(DefaultSensor::new)
                 .collect(Collectors.toList());
     }
