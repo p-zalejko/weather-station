c1c9523631f5d9d5fb61c98530976cb294143246
Pawel Zalejko <p.zalejko@gmail.com>
2021-01-11 22:59:25 +0100

    removed not needed service exporting

--- core/src/main/java/com/gmail/pzalejko/weather/core/ModuleExportedTmpDS18B20DeviceType.java
+++
@@ -1,6 +0,0 @@
-package com.gmail.pzalejko.weather.core;
-
-import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
-
-public class ModuleExportedTmpDS18B20DeviceType extends TmpDS18B20DeviceType {
-}
--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,12 +1,8 @@
-import com.gmail.pzalejko.weather.core.ModuleExportedTmpDS18B20DeviceType;
-import com.pi4j.io.w1.W1DeviceType;
-
 module pzalejko.weatchersensor {
     requires static lombok;
 
     requires pi4j.core;
     requires pi4j.device;
     requires pi4j.gpio.extension;
-    provides W1DeviceType with ModuleExportedTmpDS18B20DeviceType;
     exports com.gmail.pzalejko.weather.core;
 }
