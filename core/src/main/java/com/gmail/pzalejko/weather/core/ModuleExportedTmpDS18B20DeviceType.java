d47402cf1b9b7fe7c56cf73b50b32a804e7ac3b0
Pawel Zalejko <p.zalejko@gmail.com>
2021-01-11 22:44:33 +0100

    added export of a service

---
+++ core/src/main/java/com/gmail/pzalejko/weather/core/ModuleExportedTmpDS18B20DeviceType.java
@@ -0,0 +1,6 @@
+package com.gmail.pzalejko.weather.core;
+
+import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
+
+public class ModuleExportedTmpDS18B20DeviceType extends TmpDS18B20DeviceType {
+}
